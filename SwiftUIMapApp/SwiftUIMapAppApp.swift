a154157bbfd5a8fb25e03a19f9bf11715435ca02
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-06-16 16:48:58 +0100

    init SwiftUI project

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by DataArt Viktor Drykin on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
