b6d3cd479bccdff73861c7822b88b176055fb71c
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-06-17 16:25:43 +0100

    update ViewModel

--- SwiftUIMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftUIMapApp/ViewModels/LocationsViewModel.swift
@@ -6,14 +6,33 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
 
     @Published var locations: [Location]
 
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    private let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first! // just because we are learning SwiftUI
+        updateMapRegion(location: locations.first!)
+    }
+
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
     }
 
 }
